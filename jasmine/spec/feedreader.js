924f5e7ed5859654dfbd4788fde2ac2abfb4f6c8
Nathan Heck <36094454+MeatBot99@users.noreply.github.com>
2018-10-18 11:28:51 -0600

    Add files via upload
    As per review, I made some changes to the tests to make them more comprehensive. Thanks anonymous reviewer!

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -10,27 +10,28 @@ $(function() {
 
     //Checks that the allFeeds array of objects have a URL and that the URL is not empty.
     it('have a URL, URL not empty', function(){
-      for (let feed in allFeeds){
-      expect(feed.url).toBe();
-      expect(feed.url).not.toBe(null);
+      for (let feed of allFeeds){
+      expect(feed.url).toBeDefined();
+      expect(feed.url.length).not.toBe(0);
       }
     });
 
     //Checks that the allFeeds objects have a name and the name is not empty.
     it('have a name, name not empty', function(){
-      for (let feed in allFeeds){
-        expect(feed.name).toBe();
-        expect(feed.name).not.toBe(null);
+      for (let feed of allFeeds){
+        expect(feed.name).toBeDefined();
+        expect(feed.name.length).not.toBe(0);
       }
     });
   });
+
   //Second test suite. Focuses on menu visibility and toggling of visibility.
   describe('The Menu', function(){
     let menu= document.querySelector("body");
 
     //Checks that the menu is hidden by default; It is hidden if it has the "menu-hidden" class.
     it('is hidden by default', function(){
-      expect(menu.className).toBe('menu-hidden');
+      expect(menu.classList.contains("menu-hidden")).toBe(true);
     });
 
     //Checks that the menu changes visibility when menu icon is clicked.
@@ -39,29 +40,31 @@ $(function() {
 
       //Simulates a click on the menu icon to show the menu.
       iconButton.click();
-      expect(menu.className).not.toBe('menu-hidden');
+      expect(menu.classList.contains("menu-hidden")).not.toBe(true);
       //Simulates a click on the menu icon to hide the menu.
       iconButton.click();
-      expect(menu.className).toBe('menu-hidden');
+      expect(menu.classList.contains("menu-hidden")).toBe(true);
     });
   });
+
   //The feed container is used for the third and fourth suites. Placed in this scope for ease of accesss.
   let feed= document.querySelector(".feed");
 
   //Third test suite. Asynchonous test to ensure the feeds are not empty.
   describe('Initial Entries', function(){
-    let entry= document.querySelector(".entry");
-
     //Uses the asynchonous functionality with beforeEach method.
     beforeEach(function(done){
       loadFeed(0, done);
     });
-    //Checks that the feed has a child and that the feed child has a class of "entry".
+    //Checks that the feed has a child and that the feed child has a class of "entry". Used jQuery to ease selection of elements.
     it('has at least one .entry element', function(){
-      expect(feed.children.length).toBeGreaterThan(0);
+      let entry= feed.querySelector(".entry");
+
+      expect($('.feed .entry').length).toBeGreaterThan(0);
       expect(entry).toBeDefined();
     });
   });
+
   //Fourth test suite. Checks that when a new feed is loaded that the content changes.
   describe('New Feed Selection', function(){
     //Declare a variable to hold the value of the first feed when it loads.
